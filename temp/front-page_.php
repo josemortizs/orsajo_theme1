990a1e95b112eecab61fa4153c6e955435b0e0fc
José Manuel Ortiz Sánchez <josemortizs@gmail.com>
2019-09-17 17:47:14 +0200

    Actualizado fron-page.php

--- temp/front-page_.php
+++ temp/front-page_.php
@@ -4,6 +4,8 @@
     $site_title = get_bloginfo( 'name' );
     $site_url = network_site_url( '/' );
     $site_description = get_bloginfo( 'description' );
+    
+   // Pendiente de sección TRABAJOs
 ?>
 
 
