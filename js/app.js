42aa698217f34978fce08ba5ac5fc340f681fc42
Jose Manuel Ortiz Sanchez <josemortizs@MacBook-Pro-de-Jose.local>
2019-10-04 18:54:28 +0200

    Documentando

--- js/app.js
+++ js/app.js
@@ -1,11 +1,30 @@
+/*
+** Theme: Orsajo_theme1
+** app.js - Este archivo se carga en todas las partes del tema.
+*/
+
 const formularioBuscar = document.querySelector('#searchform #s');
 const listaEntradas = document.querySelector('.widg-dere ul');
+
+
+/*
+** En caso de existir el formulario cuyo id sea #searchform le agrega
+** las clases form-control y mb-2 (Bootstrap). Al existir formularioBuscar
+** ha de existir el botón #searchsubim, por lo que le agrega las clases
+** btn btn-primry y btn-block.
+*/
 
 if(formularioBuscar) {
     formularioBuscar.className += ' form-control mb-2';
     const botonBuscar = document.querySelector('#searchsubmit');
     botonBuscar.className += ' btn btn-primary btn-block';
 }
+
+
+/*
+** En caso de existir una lista de entradas, mediante un forEach,
+** les agrega la clase list-group-item.
+*/
 
 if(listaEntradas) {
     listaEntradas.className += ' list-group';
@@ -15,17 +34,35 @@ if(listaEntradas) {
     }); 
 }
 
-// Estilos comentarios
+
+/*
+** En caso de existir la opción de comentarios, agregamos al botón de éstos
+** la clase btn btn-primary.
+*/
+
 const botonComentarios = document.querySelector('.form-submit #submit');
 if(botonComentarios){
   botonComentarios.className = 'btn btn-primary';
 }
+
+
+/*
+** En caso de existir la opción de comentarios, agregamos al text-area de éstos
+** la clase form-control y el atributo "placeholder" con el valor: "Ingresa aquí tu comentario"
+*/
 
 const inputTextArea = document.querySelector('.comment-form-comment textarea');
 if(inputTextArea){
   inputTextArea.className = 'form-control';
   inputTextArea.setAttribute('placeholder', 'Ingresa aquí tu comentario*');
 }
+
+
+/*
+** Ocultamos las etiquetas, agregamos a los campos input la clase 
+** de Bootstrap form-control y al input o textarea un placeholder que
+** sustituya la información que ofrecía dicha etiqueta.
+*/
 
 const comentariosNuevos = document.querySelector('#commentform');
 if(comentariosNuevos
@@ -51,12 +88,24 @@ if(comentariosNuevos
 
 }
 
+
+/*
+** En caso de existir la opción de editar comentarios, agregamos al botón de éstos
+** las clases btn btn-outline-warning btn-sm mb-2.
+*/
+
 const botonEditarComentarios = document.querySelectorAll('.comment-edit-link');
 if(botonEditarComentarios){
   botonEditarComentarios.forEach(boton => {
     boton.className = 'btn btn-outline-warning btn-sm mb-2'
   })
 }
+
+
+/*
+** En caso de existir la opción de responder a un comentario, agregamos al botón de éstos
+** las clases btn btn-outline-primary btn-sm mb-2.
+*/
 
 const botonResponderComentarios = document.querySelectorAll('.comment-reply-link');
 if(botonResponderComentarios){
